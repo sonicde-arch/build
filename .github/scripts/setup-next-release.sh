445c330b43e076123b33aa48853de5089eac85d9
callmetango <callmetango@users.noreply.github.com>
2026-09-20 03:34:24 +0000

    Debugging

--- .github/scripts/setup-next-release.sh
+++ .github/scripts/setup-next-release.sh
@@ -94,7 +94,7 @@ docker exec --user "$(id -u):$(id -g)" builder sh -c '
 		test -f "$pkgbuild" || continue
 		pkgbase=${pkgbuild%/*}
 		cd "$pkgbase"
-		for asset in $(makepkg --packagelist -p "$pkgbuild"); do
+		for asset in $(makepkg --packagelist); do
 			printf "%s\n" "$asset" >> assets.csv
 			printf "%s|%s\n" "$asset" "$pkgbase" >> assets2bases.csv
 		done
