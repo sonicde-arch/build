03e3bbdd37a61ccf697954434f18bc0f998393ab
callmetango <callmetango@users.noreply.github.com>
2026-09-21 01:01:58 +0000

    Debugging

--- .github/scripts/libarchpkg.sh
+++ .github/scripts/libarchpkg.sh
@@ -66,52 +66,6 @@ makepkg_packagelist() {
 }
 
 # Arguments
-# $1: path to PKGBUILD
-pkgbuild_pkgnames() {
-	bash -s -- "$1" <<'EOF'
-		set -eu
-		source "$1"/PKGBUILD
-		printf '%s\n' "${pkgname[@]}"
-EOF
-}
-
-
-# Arguments
-# $1: path to PKGBUILD
-pkgbuild_pkgnames_to_wildcards() {
-	bash -s -- "$1" <<'EOF'
-		set -eu
-		epoch=0
-		source "$1"/PKGBUILD
-		test $epoch -gt 0 && epoch="${epoch}_" || epoch=
-		suffix="-${epoch}$pkgver-$pkgrel-*.pkg.*"
-		printf '%s\n' "${pkgname[@]}" | sed "s/$/$suffix/g"
-		printf '%s-debug\n' "${pkgname[@]}" | sed "s/$/$suffix/g"
-EOF
-}
-
-
-# Arguments
-# $1: path to repository database archive
-# $2: newline-separated package names
-repodb_has_pkgnames() {
-	repo_db=$1
-	pkgnames=$2
-
-	registered=$(
-		tar --zstd -xOf "$repo_db" --wildcards '*/desc' |
-			awk '$0 == "%NAME%" { getline; print }'
-	)
-
-	while IFS= read -r pkgname || [ -n "$pkgname" ] ; do
-		[ -n "$pkgname" ] || continue
-		printf '%s\n' "$registered" | grep -Fqx "$pkgname" || return 1
-	done <<EOF
-$pkgnames
-EOF
-}
-
-# Arguments
 # $1: docker image
 # $2: path to repository database
 # $3-$n: package files to add
